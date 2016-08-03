71108c7546e9ea201b111d763abfaa7d0bc3b95b
lomaxrx <alomax@americanprogress.org>
2016-08-03 17:36:29 -0400

    switched to relative path

--- public/app.js
+++ public/app.js
@@ -69350,7 +69350,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var ENDPOINT = exports.ENDPOINT = '/data';
+var ENDPOINT = exports.ENDPOINT = 'data';
 
 
 var CHILD_CARE_CENTER_FILE_NAME = 'childCareCenters.tsv';
@@ -69467,7 +69467,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var ENDPOINT = exports.ENDPOINT = '/data';
+var ENDPOINT = exports.ENDPOINT = 'data';
 
 
 var STATE_FILE_NAME = 'states.json';
